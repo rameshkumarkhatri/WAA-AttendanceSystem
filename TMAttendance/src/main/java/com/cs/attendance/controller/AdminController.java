b93d024aa58c96f4511580e3685526b491929cc8
SamuelPaulos <samuel.paulos@yahoo.com>
2018-11-19 20:45:29 -0600

    Controllers refined

--- TMAttendance/src/main/java/com/cs/attendance/controller/AdminController.java
+++ TMAttendance/src/main/java/com/cs/attendance/controller/AdminController.java
@@ -1,8 +1,62 @@
 package com.cs.attendance.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cs.attendance.domain.AttendanceDTO;
+import com.cs.attendance.domain.TMAttendance;
+
 @RestController
+@RequestMapping("/admin")
 public class AdminController {
 
+	@RequestMapping("/delete/{sid}")
+	public ResponseEntity<String> deleteTMAttendance(@PathVariable("sid") String studentId) {
+
+		return new ResponseEntity<String>("", HttpStatus.OK);
+	}
+
+	@PutMapping("/update/{sid}")
+	public ResponseEntity<String> updateTMAttendance(@PathVariable("sid") String studentId,
+			@RequestBody TMAttendance tmAttendance) {
+
+		return new ResponseEntity<String>("", HttpStatus.OK);
+	}
+
+	@PostMapping("/create")
+	public ResponseEntity<String> createTMAttendance(@RequestBody TMAttendance tmAttendance) {
+
+		return new ResponseEntity<String>("", HttpStatus.OK);
+	}
+
+	@RequestMapping("/get/attendance/{id}/{year}/{month}/{date}/{shift}")
+	public ResponseEntity<AttendanceDTO> getStudentAttendace(@PathVariable("id") String studentId,
+			@PathVariable("date") int date, @PathVariable("month") int month, @PathVariable("year") int year,
+			@PathVariable("shift") String shift) {
+		return null;
+	}
+
+	@RequestMapping("/get/attendance/{bid}/{sid}")
+	public ResponseEntity<List<AttendanceDTO>> getStudentBlockAttendance(@PathVariable("sid") String studentId,
+			@PathVariable("bid") String blockId) {
+		return null;
+	}
+
+	@RequestMapping("/get/attendance/{sid}")
+	public ResponseEntity<List<AttendanceDTO>> getAllStudentsAttendance(@PathVariable("sid") String studentId) {
+		return null;
+	}
+
+	@RequestMapping("/get/attendance/{fid}")
+	public ResponseEntity<List<AttendanceDTO>> getfacultyStudentsBlockAttendance(@PathVariable("fid") String studentId) {
+		return null;
+	}
 }
